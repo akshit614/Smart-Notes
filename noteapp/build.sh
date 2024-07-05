1fcb0486af4aad39551ecf0fd8dbdcf310cbf203
Aksh4code <akkisingh8266@gmail.com>
2024-07-05 12:52:31 +0530

    Update build.sh

--- noteapp/build.sh
+++ noteapp/build.sh
@@ -1,5 +1,5 @@
 # Install MySQL client libraries
-apt-get update && apt-get install -y libmysqlclient-dev
+pip install -y libmysqlclient-dev
 
 # Install Python dependencies
 pip install -r requirements.txt
